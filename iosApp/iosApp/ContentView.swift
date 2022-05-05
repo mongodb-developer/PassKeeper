8da9255541a9792c0ac70b351afefafda34e7328
Mohit Sharma <mohit7575@gmail.com>
2022-05-05 10:19:29 +0530

    integration with PassGen

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,14 +3,33 @@ import shared
 
 struct ContentView: View {
 	let greet = Greeting().greeting()
+    
+    @State var randomPassword = PasswordGeneratorKt.getNewPassword(length: 13)
 
 	var body: some View {
-		Text(greet)
+        
+        VStack{
+            
+            Text(randomPassword)
+            
+            Button("Refresh") {
+                self.randomPassword = PasswordGeneratorKt.getNewPassword(length: 13)
+            }
+            .padding(10)
+            .overlay(
+               RoundedRectangle(cornerRadius: 8)
+                   .stroke(Color.blue, lineWidth: 1)
+           )
+        }
+        
+        
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+        Group {
+            ContentView()
+        }
 	}
 }
